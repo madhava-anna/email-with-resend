6a3e00bfd6447bd079817a3defab3a6282d11144
madhava.reddy@innosys.ai <madhava.reddy@innosys.ai>
2025-09-29 11:22:57 +0100

    req json

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import { Resend } from "resend";
 export default {
   async fetch(request, env, ctx) {
     const resend = new Resend(env.RESEND_API_KEY);
-
+	req = await request.json()
     const { data, error } = await resend.emails.send({
-      from: request["email"],
+      from: req["email"],
       to: "contact@innosys.ai",
-      subject: request["name"] + " from "+request["company"],
-      html: request["message"],
+      subject: req["name"] + " from "+request["company"],
+      html: req["message"],
     });
 
     return Response.json({ data, error });
