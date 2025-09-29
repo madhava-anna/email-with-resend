6e69570f4f3951ccb6d454ebb1434da6b2c41f74
madhava-anna <madhava.reddy@innosys.ai>
2025-09-29 12:27:14 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 export default {
   async fetch(request, env, ctx) {
     const resend = new Resend(env.RESEND_API_KEY);
-	req = await request.json()
+	const req = await request.json()
     const { data, error } = await resend.emails.send({
       from: req["email"],
       to: "contact@innosys.ai",
